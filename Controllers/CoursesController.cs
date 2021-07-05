817f8e322e081d97261ccac4673c2e55abb48a5f
NhatTuyen00 <83211886+NhatTuyen00@users.noreply.github.com>
2021-07-05 17:19:37 +0700

    commit lan 2

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -44,7 +44,7 @@ namespace BigSchool.Controllers
                 LecturerId = User.Identity.GetUserId(),
                 DateTime = viewModel.GetDateTime(),
                 CategoryId = viewModel.Category,
-                Place = viewModel.Place,
+                Place = viewModel.Place
             };
             _dbContext.Courses.Add(course);
             _dbContext.SaveChanges();
